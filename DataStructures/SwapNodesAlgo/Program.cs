43aa9aa8e2d3b3c93a9043baeb233ec0afcd605e
Ted Kalaitzidis <thodwriskalaitzidis@hotmail.com>
2016-08-02 00:22:52 +0300

    Almost done with swapNodes

--- DataStructures/SwapNodesAlgo/Program.cs
+++ DataStructures/SwapNodesAlgo/Program.cs
@@ -12,7 +12,110 @@ class Program
 {
     static void Main(string[] args)
     {
-        int NumOfNodes = int.Parse(Console.ReadLine());
+        string[] lines = new string[]
+            {
+                "11",
+                "2 3",
+                "4 -1",
+                "5 -1",
+                "6 -1",
+                "7 8",
+                "-1 9",
+                "-1 -1",
+                "10 11",
+                "-1 -1",
+                "-1 -1",
+                "-1 -1",
+            },
+            swaps = new string[]
+            {
+                "2",
+                "2",
+                "4",
+            };
+
+        int NumOfNodes = int.Parse(lines[0]);//int.Parse(Console.ReadLine());
+        Node tree = NumOfNodes == 0 ? null : new Node(1);
+        int currentDepth = 2;
+        for (var i = 0; i < NumOfNodes; i++)
+        {
+            var line = Array.ConvertAll(lines[i + 1].Split(' '), int.Parse); //Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse)
+            while (!AppendToTree(tree, currentDepth, 1, line[0], line[1]))
+                currentDepth++;
+        }
+
+        int NumOfSwaps = int.Parse(swaps[0]);// int.Parse(Console.ReadLine());
+        for (var i = 0; i < NumOfSwaps; i++)
+        {
+            int initialDepth = int.Parse(swaps[i + 1]),// int.Parse(Console.ReadLine()),
+                depth = initialDepth;
+            while (Swap(tree, depth, 1))
+            {
+                depth += initialDepth;
+                InorderPrint(tree);
+            }
+            
+        }
+    }
+
+    /// <summary>
+    /// Appends left and right data to a node at the desired depth. returns true if children created and false if not
+    /// </summary>0
+    /// <returns></returns>
+    static bool AppendToTree(Node node, int desiredDepth, int currentDepth, int dataLeft, int dataRight)
+    {
+        bool result = false;
+        if ((currentDepth + 1) == desiredDepth)
+        {
+            if (node.CreatedChildren)
+                return false;
+            if (node.Left == null && dataLeft != -1)
+                node.Left = new Node(dataLeft);
+            if (node.Right == null && dataRight != -1)
+                node.Right = new Node(dataRight);
+            result = true;
+            node.CreatedChildren = true;
+        }
+        else
+        {
+            if (node.Left != null)
+                result = AppendToTree(node.Left, desiredDepth, currentDepth + 1, dataLeft, dataRight);
+            if (!result && node.Right != null)
+                result = AppendToTree(node.Right, desiredDepth, currentDepth + 1, dataLeft, dataRight);
+        }
+        return result;
+    }
+
+    /// <summary>
+    /// Swaps the children of nodes at the desired depth. Returns true if any swap performed, false otherwise
+    /// </summary>
+    static bool Swap(Node node, int desiredDepth, int currentDepth)
+    {
+        bool result = false;
+        if (currentDepth == desiredDepth)
+        {
+            var temp = node.Left;
+            node.Left = node.Right;
+            node.Right = temp;
+            result = true;
+        }
+        else if (currentDepth < desiredDepth)
+        {
+            if (node.Left != null)
+                result = Swap(node.Left, desiredDepth, currentDepth + 1);
+            if (node.Right != null)
+                result = result || Swap(node.Right, desiredDepth, currentDepth + 1);
+        }
+        return result;
+    }
+
+    static void InorderPrint(Node node)
+    {
+        if (node.Left != null)
+            InorderPrint(node.Left);
+        Console.Write("{0} ", node.Data);
+        if (node.Right != null)
+            InorderPrint(node.Right);
     }
 }
 
@@ -21,6 +124,7 @@ class Node
     public int Data;
     public Node Left;
     public Node Right;
+    public bool CreatedChildren;
 
     public Node(int data)
     {
